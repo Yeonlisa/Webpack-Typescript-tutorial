d0c6cf00ef952d6b5514df5a0e4ffe85996c9c2f
yeonlisa <tkzw4869@gmail.com>
2022-02-24 13:56:00 +0900

    Using ES6 Modules

--- webpack.config.js
+++ webpack.config.js
@@ -4,16 +4,19 @@ module.exports = {
     entry: './src/index.ts',
     module: {
         rules: [
-            {
-                test: /\.ts$/,
-                use: 'ts-loader',
-                include: [path.resolve(__dirname, 'src')]
-            }
+        {
+            test: /\.ts$/,
+            include: [path.resolve(__dirname, 'src')],
+            use: 'ts-loader',
+        }
         ]
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
     },
     output: {
         publicPath: 'public',
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'public')
-    }
-}
+        path: path.resolve(__dirname, 'public'),
+    },
+};
